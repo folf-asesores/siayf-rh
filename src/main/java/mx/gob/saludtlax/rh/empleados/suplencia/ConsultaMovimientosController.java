bd18adce495f96457ed2ef9a24a90864bd18f99b
Luis Miguel <Luis Miguel@LuisMiguel-HP>
2017-04-26 00:52:48 -0500

    Doble ManageBean

--- src/main/java/mx/gob/saludtlax/rh/empleados/suplencia/ConsultaMovimientosController.java
+++ src/main/java/mx/gob/saludtlax/rh/empleados/suplencia/ConsultaMovimientosController.java
@@ -1,35 +1,35 @@
-package mx.gob.saludtlax.rh.empleados.suplencia;
-
-import java.io.Serializable;
-
-import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-import javax.inject.Inject;
-
-import mx.gob.saludtlax.rh.util.SelectItemsUtil;
-
-@ManagedBean(name = "consultaMovimiento")
-@ViewScoped
-public class ConsultaMovimientosController implements Serializable {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 8659315013652832562L;
-	@Inject
-	private Suplencia suplencia;
-
-	private ConsultaMovimientoView view = new ConsultaMovimientoView();
-
-	@PostConstruct
-	public void inicio() {
-		view.setMostrarBusqueda(true);
-		view.setListaMovimientos(SelectItemsUtil.listaTiposMovimientosSuplentes());
-	}
-
-	public void consultarMovimiento() {
-		view.setMovimientos(suplencia.consultarMovimientosSuplente(view.getFiltro()));
-	}
-
-}
+//package mx.gob.saludtlax.rh.empleados.suplencia;
+//
+//import java.io.Serializable;
+//
+//import javax.annotation.PostConstruct;
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.ViewScoped;
+//import javax.inject.Inject;
+//
+//import mx.gob.saludtlax.rh.util.SelectItemsUtil;
+//
+//@ManagedBean(name = "consultaMovimiento")
+//@ViewScoped
+//public class ConsultaMovimientosController implements Serializable {
+//
+//	/**
+//	 * 
+//	 */
+//	private static final long serialVersionUID = 8659315013652832562L;
+//	@Inject
+//	private Suplencia suplencia;
+//
+//	private ConsultaMovimientoView view = new ConsultaMovimientoView();
+//
+//	@PostConstruct
+//	public void inicio() {
+//		view.setMostrarBusqueda(true);
+//		view.setListaMovimientos(SelectItemsUtil.listaTiposMovimientosSuplentes());
+//	}
+//
+//	public void consultarMovimiento() {
+//		view.setMovimientos(suplencia.consultarMovimientosSuplente(view.getFiltro()));
+//	}
+//
+//}
