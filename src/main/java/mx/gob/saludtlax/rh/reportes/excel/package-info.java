d2dbfbf7b6cd5ca80bf640201d7aa89570f51f33
Freddy Barrera <freddy.barrera.moo@gmail.com>
2017-11-19 19:53:23 -0600

    Información de paquete
    Agregue la información del paquet de reportes de Excel

---
+++ src/main/java/mx/gob/saludtlax/rh/reportes/excel/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * package-info.java
+ * Creado el 19/nov/2017 06:56:31 p. m.
+ *
+ */
+
+/**
+ * Este paquete contiene las clases que describen como se debe implementar un
+ * reporte de excel.
+ */
+
+package mx.gob.saludtlax.rh.reportes.excel;
